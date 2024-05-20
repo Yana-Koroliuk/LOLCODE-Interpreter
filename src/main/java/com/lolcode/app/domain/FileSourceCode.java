33a8a39187865df1b21a4d5abadb2ece031fed56
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-05-20 18:47:55 +0200

    Implement `FileSourceCode`

--- src/main/java/com/lolcode/app/domain/FileSourceCode.java
+++ src/main/java/com/lolcode/app/domain/FileSourceCode.java
@@ -1,12 +1,32 @@
 package com.lolcode.app.domain;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
 public class FileSourceCode implements SourceCode {
-    
-    public FileSourceCode(String path) {
-        throw new UnsupportedOperationException("Method not implemented!");
+    private final Path path;
+    private final List<String> lines;
+
+    public FileSourceCode(String fileName) throws IOException {
+        path = Paths.get(fileName);
+        lines = Collections.unmodifiableList(Files.readAllLines(path));
+    }
+
+    @Override
+    public String getModuleName() {
+        return path.getFileName().toString();
+    }
+
+    @Override
+    public List<String> getLines() {
+        return lines;
     }
 }
