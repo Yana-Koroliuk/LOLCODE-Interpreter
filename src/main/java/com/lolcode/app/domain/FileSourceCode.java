7710a71c0d097b36c58f65816ad424ecbba3e490
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-05-20 18:47:55 +0200

    Wrap `FileSourceCode` creation with try-catch

--- src/main/java/com/lolcode/app/domain/FileSourceCode.java
+++ src/main/java/com/lolcode/app/domain/FileSourceCode.java
@@ -1,12 +1,36 @@
 package com.lolcode.app.domain;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
 public class FileSourceCode implements SourceCode {
-    
-    public FileSourceCode(String path) {
-        throw new UnsupportedOperationException("Method not implemented!");
+    private final Path path;
+    private final List<String> lines;
+
+    public FileSourceCode(String fileName) {
+        try {
+            path = Paths.get(fileName);
+            lines = Collections.unmodifiableList(Files.readAllLines(path));
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Can't read file: " + fileName, e);
+        }
+    }
+
+    @Override
+    public String getModuleName() {
+        return path.getFileName().toString();
+    }
+
+    @Override
+    public List<String> getLines() {
+        return lines;
     }
 }
--- src/main/java/com/lolcode/app/domain/SourceCode.java
+++ src/main/java/com/lolcode/app/domain/SourceCode.java
@@ -1,8 +1,14 @@
 package com.lolcode.app.domain;
 
+import java.util.List;
+
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
 public interface SourceCode {
+
+    String getModuleName();
+    
+    List<String> getLines();
 }
