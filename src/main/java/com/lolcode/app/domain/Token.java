29d8c9476b28370a6342b43e426169c0f3b19c37
Mykhailo Balakhon <9mohapx9@gmail.com>
2024-05-20 18:31:05 +0200

    Implement `Token` structure

--- src/main/java/com/lolcode/app/domain/Token.java
+++ src/main/java/com/lolcode/app/domain/Token.java
@@ -1,10 +1,12 @@
 package com.lolcode.app.domain;
 
-import java.util.ArrayList;
-
 /**
  * @author Mykhailo Balakhon
  * @link <a href="mailto:mykhailo.balakhon@communify.us">mykhailo.balakhon@communify.us</a>
  */
-public class ByteCode extends ArrayList<Token> {
+public record Token(Type type, String value, int line) {
+
+    public enum Type {
+        KEYWORD, NUMBER, NEWLINE, IDENTIFIER, STRING
+    }
 }
